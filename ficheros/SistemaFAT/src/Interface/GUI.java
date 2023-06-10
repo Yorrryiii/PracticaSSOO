17c0f8812f48942f2171112689f49d9fb718761f
Yorrryiii <94568320+Yorrryiii@users.noreply.github.com>
2023-06-10 04:19:53 +0200

    Implementación esqueleto no funcional de la interfaz. Falta definir las fucinonalidades de los botones y hacer la cantidad de clusters que se dibujan dinámica

--- ficheros/SistemaFAT/src/Interface/GUI.java
+++ ficheros/SistemaFAT/src/Interface/GUI.java
@@ -1,4 +1,5 @@
 package Interface;
+
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.table.DefaultTableModel;
@@ -10,42 +11,65 @@ public class GUI extends JFrame {
         setSize(2500, 1500);
         setLayout(new BorderLayout());
 
-        // Panel derecho (50%)
+        // Panel derecho (68%)
         JPanel panelDerecho = new JPanel();
-        panelDerecho.setBackground(Color.YELLOW);
+        panelDerecho.setBackground(Color.WHITE);
         add(panelDerecho, BorderLayout.EAST);
-        panelDerecho.setPreferredSize(new Dimension(getWidth() / 2, getHeight()));
+        panelDerecho.setPreferredSize(new Dimension(getWidth() * 68 / 100, getHeight()));
 
-        // Panel central (30%)
-        JPanel panelCentral = new JPanel(new BorderLayout());
-        panelCentral.setBackground(Color.GREEN);
-        add(panelCentral, BorderLayout.CENTER);
-        panelCentral.setPreferredSize(new Dimension(getWidth() * 3 / 10, getHeight()));
+        // Panel para los bloques y el fragmento de texto
+        JPanel panelBloques = new JPanel(new BorderLayout());
+        panelBloques.setBackground(Color.WHITE);
+        panelDerecho.add(panelBloques, BorderLayout.CENTER);
 
-        // Crear la tabla
-        String[] columnNames = {"NOMBRE", "TIPO", "Ci"};
-        Object[][] data = new Object[20][3];
-        DefaultTableModel model = new DefaultTableModel(data, columnNames);
-        JTable table = new JTable(model);
-        JScrollPane scrollPane = new JScrollPane(table);
-        panelCentral.add(scrollPane, BorderLayout.CENTER);
+        // Etiqueta "SECCION CLUSTERS" en la parte superior de la columna derecha
+        JLabel labelSeccionClusters = new JLabel("SECCION CLUSTERS");
+        labelSeccionClusters.setFont(new Font("Arial", Font.BOLD, 25));
+        labelSeccionClusters.setHorizontalAlignment(SwingConstants.CENTER);
+        labelSeccionClusters.setOpaque(true);
+        labelSeccionClusters.setBackground(Color.decode("#FFE167"));
+        panelBloques.add(labelSeccionClusters, BorderLayout.NORTH);
 
-        // Panel izquierdo (20%)
+        // Panel para los bloques
+        JPanel panelMatriz = new JPanel(new GridLayout(5, 5, 10, 10));
+        panelMatriz.setBackground(Color.WHITE);
+        panelBloques.add(panelMatriz, BorderLayout.CENTER);
+
+        // Crear bloques (casillas)
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                JPanel bloque = new JPanel();
+                bloque.setPreferredSize(new Dimension(100, 100));
+                bloque.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                panelMatriz.add(bloque);
+            }
+        }
+
+        // Panel izquierdo (30%)
         JPanel panelIzquierdo = new JPanel(new BorderLayout());
         panelIzquierdo.setBackground(Color.BLUE);
         add(panelIzquierdo, BorderLayout.WEST);
-        panelIzquierdo.setPreferredSize(new Dimension(getWidth() / 5, getHeight()));
+        panelIzquierdo.setPreferredSize(new Dimension(getWidth() * 3 / 10, getHeight()));
+
+        // Primera fila de la columna izquierda
+        JPanel panelFila1 = new JPanel(new BorderLayout());
+        panelIzquierdo.add(panelFila1, BorderLayout.NORTH);
 
         // Etiqueta "OPCIONES"
         JLabel labelOpciones = new JLabel("OPCIONES");
-        labelOpciones.setFont(new Font("Arial", Font.BOLD, 20));
+        labelOpciones.setFont(new Font("Arial", Font.BOLD, 25));
         labelOpciones.setHorizontalAlignment(SwingConstants.CENTER);
-        panelIzquierdo.add(labelOpciones, BorderLayout.NORTH);
+        labelOpciones.setOpaque(true);
+        labelOpciones.setBackground(Color.decode("#FFE167"));
+        panelFila1.add(labelOpciones, BorderLayout.NORTH);
 
         // Panel para los botones
         JPanel panelBotones = new JPanel();
-        panelBotones.setLayout(new BoxLayout(panelBotones, BoxLayout.Y_AXIS));
-        panelIzquierdo.add(panelBotones, BorderLayout.CENTER);
+        panelBotones.setLayout(new GridLayout(2, 3, 10, 10));
+        panelBotones.setBackground(Color.BLUE);
+
+        // Espacio entre botones
+        panelBotones.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         // Botones
         JButton btnCrear = new JButton("CREAR");
@@ -55,8 +79,9 @@ public class GUI extends JFrame {
         JButton btnLimpiar = new JButton("LIMPIAR");
         JButton btnNuevo = new JButton("NUEVO");
 
-        // Ajustar tamaño de los botones
-        Dimension buttonSize = new Dimension(btnCrear.getPreferredSize().width * 3, btnCrear.getPreferredSize().height);
+        // Ajustar tamaño de los botones (el doble de grandes)
+        Dimension buttonSize = new Dimension(btnCrear.getPreferredSize().width * 2,
+                btnCrear.getPreferredSize().height * 2);
         btnCrear.setPreferredSize(buttonSize);
         btnBorrar.setPreferredSize(buttonSize);
         btnCopiar.setPreferredSize(buttonSize);
@@ -70,6 +95,29 @@ public class GUI extends JFrame {
         panelBotones.add(btnMover);
         panelBotones.add(btnLimpiar);
         panelBotones.add(btnNuevo);
+
+        JScrollPane scrollPane = new JScrollPane(panelBotones); // Envolver el panel de botones en un JScrollPane
+        panelFila1.add(scrollPane, BorderLayout.CENTER);
+
+        // Segunda fila de la columna izquierda (contenido de la antigua columna central)
+        JPanel panelFila2 = new JPanel(new BorderLayout());
+        panelIzquierdo.add(panelFila2, BorderLayout.CENTER);
+
+        // Etiqueta "Directorio Raiz"
+        JLabel labelDirectorioRaiz = new JLabel("DIRECTORIO RAIZ");
+        labelDirectorioRaiz.setFont(new Font("Arial", Font.BOLD, 25));
+        labelDirectorioRaiz.setHorizontalAlignment(SwingConstants.CENTER);
+        labelDirectorioRaiz.setOpaque(true);
+        labelDirectorioRaiz.setBackground(Color.decode("#FFE167"));
+        panelFila2.add(labelDirectorioRaiz, BorderLayout.NORTH);
+
+        // Crear la tabla
+        String[] columnNames = { "NOMBRE", "TIPO", "Ci" };
+        Object[][] data = new Object[20][3];
+        DefaultTableModel model = new DefaultTableModel(data, columnNames);
+        JTable table = new JTable(model);
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        panelFila2.add(tableScrollPane, BorderLayout.CENTER);
 
         setVisible(true);
     }
